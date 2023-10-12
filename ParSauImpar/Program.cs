b3604a716e27d9ea450f858ca65e6a4472735659
briansomogyi <brian.somogyi@emanuel.ro>
2023-10-12 15:46:16 +0300

    Done some changes

---
+++ ParSauImpar/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ParSauImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("a="); a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("par");
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
+        }
+    }
+}
